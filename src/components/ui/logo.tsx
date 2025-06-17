c9a5f9b36ce9e187d1452ecb63f3460cd9a4926e
Tec94 <63695678+Tec94@users.noreply.github.com>
2025-06-17 14:06:00 -0500

    - adjust logo size - adjust navbar visibility

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Logo = () => (
-  <img src="/logos/smartnest_logo.png" alt="SmartNest Logo" className="h-10 w-auto invert drop-shadow-lg" />
+  <img src="/logos/smartnest_logo.png" alt="SmartNest Logo" className="h-12 w-auto invert drop-shadow-lg" />
 );
 
 export default Logo; 
