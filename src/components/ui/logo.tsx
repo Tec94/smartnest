230d68bc1e030acfb1d75532a51da21186adbeb1
Tec94 <63695678+Tec94@users.noreply.github.com>
2025-06-17 13:47:29 -0500

    - replace placeholder logo with image

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 
 const Logo = () => (
-  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="32" height="32" rx="8" fill="#0A2540"/>
-    <path d="M9 22V10L16 16L23 10V22" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-  </svg>
+  <img src="/logos/smartnest_logo.png" alt="SmartNest Logo" className="h-10 w-auto invert drop-shadow-lg" />
 );
 
 export default Logo; 
