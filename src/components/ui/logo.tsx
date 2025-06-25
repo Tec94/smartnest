85868b8af6ae8a91fb9b699b4db5adb9a5faf40d
Tec94 <63695678+Tec94@users.noreply.github.com>
2025-06-25 06:27:09 -0500

    optimize images and seo

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Logo = () => (
-  <img src="/logos/smartnest_logo.png" alt="SmartNest Logo" className="h-12 w-auto invert drop-shadow-lg" />
+  <img src="/logos/smartnest_logo.webp" alt="SmartNest Logo" className="h-12 w-auto invert drop-shadow-lg" width="120" height="48" />
 );
 
 export default Logo; 
